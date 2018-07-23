cf5602a0862755140193c12cd833dc30720fd022
wjl <xo@xo.com>
2018-07-23 12:44:17 +0800

    第一次提交

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import state from './state'
+import mutations from './mutations'
+import getters from './getters'
+import actions from './actions'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+	state,
+	mutations,
+	getters,
+	actions
+})
+export default store
