cd2bc2032eb032004c4972016549277a51377a2f
wjl <xo@xo.com>
2018-07-23 13:02:19 +0800

    2

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import state from './state'
 import mutations from './mutations'
 import getters from './getters'
 import actions from './actions'
+import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
